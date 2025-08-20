0a48afb655ef85a952f3c0d1b6788973a59a0ade
sai <sai_prasad_veluri@yahoo.com>
2025-08-20 06:27:38 +0000

    Add project files.

---
+++ AttributeRouting/Controllers/ProductsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttributeRouting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet("all")]
+        [HttpGet("listAllProducts")]
+        public IActionResult GetProducts()
+        {
+
+            return Ok(new[] {"Laptop","Mobile","Charger","Cable" });
+
+        }
+
+        [HttpGet("FindProductByName/{name?}")]
+        public IActionResult GetProducts(string? name="Mobile")
+        {
+            string[] prods = new string[] { "Laptop", "Mobile", "Charger", "Cable" };
+
+            bool found=prods.Contains(name);
+            if (found)
+            {
+                return Ok(name);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+
+        }
+
+        [HttpGet("FindName/id")]
+        public IActionResult GetProd(int id)
+        {
+            string[] prods = new string[] { "Laptop", "Mobile", "Charger", "Cable" };
+
+            string s=prods[id];
+            if (s!=null)
+            {
+                return Ok(s);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+
+        }
+
+        [HttpGet("Find/id/name")]
+        public IActionResult GetProductByIDorName(int? id,string? name)
+        {
+            string[] prods = new string[] { "Laptop", "Mobile", "Charger", "Cable" };
+            if (id == null && name!=null)
+            {
+
+                Boolean ans = prods.Contains(name);
+                return Ok($"Found by name ={name}, because id was not given");
+
+            }
+            else if (id != null)
+            {
+                int? prodid = id;
+                  return RedirectToAction("GetProd", new {id=prodid });
+                
+
+
+
+            }
+            else
+            {
+
+                return NotFound();
+            }
+
+
+        }
+
+
+
+
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            //logic to delete
+            return Ok($"ProductID {id} deleted successfully");
+
+        }
+
+    }
+}
